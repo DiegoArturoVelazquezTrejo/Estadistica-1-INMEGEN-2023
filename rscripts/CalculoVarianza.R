567bfa7f396128a139d0e23410f2543b67b50f7a
velazquez@ciencias.unam.mx <velazquez@ciencias.unam.mx>
2023-08-22 18:01:11 -0600

    Primera clase

---
+++ rscripts/CalculoVarianza.R
@@ -0,0 +1,8 @@
+# Ejemplo de un vector de datos
+datos <- c(10, 15, 20, 25, 30)
+
+# Cálculo de la varianza muestral
+varianza_muestral <- var(datos)
+
+# Imprimir la varianza muestral
+print(varianza_muestral)
---
+++ rscripts/CálculoMedia.R
@@ -0,0 +1,9 @@
+
+# Ejemplo de un vector de datos
+datos <- c(10, 15, 20, 25, 30)
+
+# Cálculo de la media
+media <- mean(datos)
+
+# Imprimir la media
+print(media)
